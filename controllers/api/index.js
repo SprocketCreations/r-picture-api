167a820815d61bb163981efffb7df7b1bc428a8e
Kailen James <sevenflags57@gmail.com>
2023-03-11 12:01:29 -0800

    New jwt system is working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -20,44 +20,35 @@ router.use("/picture", pictureRoutes)
 const commentRoutes = require("./comment")
 router.use("/comment", commentRoutes)
 
-const likeRoutes = require("./like")
+const likeRoutes = require("./like");
+const { signUser } = require('../../utils/jwt');
 router.use("/like", likeRoutes)
 
 router.post("/signin", async (req, res) => {
-    try {
-        if (!req.body.email) {
-            res.sendStatus(400)
-        } else if (!req.body.password) {
-            res.sendStatus(400)
-        }
-        const user = await User.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-        if (!user) {
-            res.sendStatus(403)
-        } else if (!bcrypt.compareSync(req.body.password, user.password)) {
-            res.sendStatus(403)
-        } else {
-            const token = jwt.sign({
-                id: user.id,
-                displayName: user.displayName
-            }, process.env.JWT_SECRET, {
-                expiresIn: "6h"
-            })
-            console.log(token)
-            res.status(201).json(
-                {
-                token: token,
-                user: user
-                }
-            )
-        }
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
+	try {
+		if (!req.body.email || !req.body.password) {
+			return res.sendStatus(400)
+		}
+
+		const user = await User.findOne({
+			where: {
+				email: req.body.email
+			}
+		});
+
+		if (!user) {
+			return res.sendStatus(403)
+		}
+
+		if (!bcrypt.compareSync(req.body.password, user.password)) {
+			return res.sendStatus(403)
+		}
+
+		return res.status(201).json({ token: signUser(user.id) });
+	} catch (err) {
+		console.log(err);
+		return res.sendStatus(500);
+	}
 })
 
 module.exports = router;
--- controllers/api/user.js
+++ controllers/api/user.js
@@ -3,320 +3,320 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const { User, Picture, Gallery, Comment, Like } = require("../../models");
 const jwt = require("jsonwebtoken");
+const { signUser } = require("../../utils/jwt");
 
 //Get 1 user
 router.get("/:userId", async (req, res) => {
-    try {
-        const user = await User.findByPk(req.params.userId)
-        if (!user) {
-            res.sendStatus(404)
-        } 
-        const token = req.headers?.authorization?.split(" ")[1];
-        if (!token) {
-            res.sendStatus(403)
-        }
-        try {
-            const data = jwt.verify(token, process.env.JWT_SECRET)
-            if (data.id == req.params.userId) {
-                res.status(201).json(user)
-            }else {
-                res.sendStatus(403)
-            }
-        } catch (err) {
-            console.log(err);
-            return res.status(403).json({ msg: "Invalid or missing token" })
-        }
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
+	try {
+		if (req.jwt.userId !== parseInt(req.params.userId)) {
+			return res.sendStatus(403);
+		}
+
+		const user = await User.findByPk(req.params.userId)
+		if (!user) {
+			return res.sendStatus(404);
+		}
+
+		return res.status(201).json({
+			displayName: user.displayName
+		});
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
 })
 
 //Create a user
 router.post("/", async (req, res) => {
-    try {
-        if (!(req.body.displayName)) {
-            return res.sendStatus(400)
-        } else if (!(req.body.email)) {
-            return res.sendStatus(400)
-        } else if (!(req.body.password)) {
-            return res.sendStatus(400)
-        }
-        const user = await User.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-        if (user) {
-            return res.sendStatus(422)
-        }
-        const newUser = await User.create(req.body)
-        res.status(201).json(newUser)
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
+	try {
+		if (!(req.body.displayName) || !(req.body.email) || !(req.body.password)) {
+			return res.sendStatus(400)
+		}
+
+		{// Check email isn't taken
+			const user = await User.findOne({
+				where: {
+					email: req.body.email
+				}
+			});
+			if (user) return res.sendStatus(422)
+
+		} {// Create new user
+			const user = await User.create({
+				displayName: req.body.displayName,
+				email: req.body.email,
+				password: req.body.password,
+				bio: req.body.bio,
+			});
+			return res.status(201).json({
+				jwt: signUser(user.id)
+			});
+		}
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
 })
 
 //Update a user
 router.put("/:userId", async (req, res) => {
-    try {
-        const findUser = await User.findByPk(req.params.userId)
-        if (!findUser) {
-            return res.sendStatus(404)
-        }
-        if (req.body.newPassword && !req.body.oldPassword) {
-            return res.sendStatus(400)
-        }
-        else if (req.body.newPassword == req.body.oldPassword) {
-            return res.sendStatus(422)
-        }
-        const user = await User.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-        if (user) {
-            return res.sendStatus(422)
-        }
-        const newUser = await User.update({
-            email: req.body.email,
-            password: req.body.newPassword,
-            displayName: req.body.displayName
-        }, {
-            where: {
-                id: req.params.userId
-            }
-        })
-        const token = req.headers?.authorization?.split(" ")[1];
-        if (!token) {
-            res.sendStatus(403)
-        }
-        try {
-            const data = jwt.verify(token, process.env.JWT_SECRET)
-            if (data.id == req.params.userId) {
-                res.status(201).json(newUser)
-            }
-        } catch (err) {
-            console.log(err);
-            return res.status(403).json({ msg: "Invalid or missing token" })
-        }
-        res.json(newUser)
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
+	try {
+		const findUser = await User.findByPk(req.params.userId)
+		if (!findUser) {
+			return res.sendStatus(404)
+		}
+		if (req.body.newPassword && !req.body.oldPassword) {
+			return res.sendStatus(400)
+		}
+		else if (req.body.newPassword == req.body.oldPassword) {
+			return res.sendStatus(422)
+		}
+		const user = await User.findOne({
+			where: {
+				email: req.body.email
+			}
+		})
+		if (user) {
+			return res.sendStatus(422)
+		}
+		const newUser = await User.update({
+			email: req.body.email,
+			password: req.body.newPassword,
+			displayName: req.body.displayName
+		}, {
+			where: {
+				id: req.params.userId
+			}
+		})
+		const token = req.headers?.authorization?.split(" ")[1];
+		if (!token) {
+			res.sendStatus(403)
+		}
+		try {
+			const data = jwt.verify(token, process.env.JWT_SECRET)
+			if (data.id == req.params.userId) {
+				res.status(201).json(newUser)
+			}
+		} catch (err) {
+			console.log(err);
+			return res.status(403).json({ msg: "Invalid or missing token" })
+		}
+		res.json(newUser)
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
 })
 
 //Delet a user
 router.delete("/:userId", async (req, res) => {
-    try {
-        const user = await User.findByPk(req.params.userId)
-        if (!user) {
-            res.sendStatus(404)
-        }
-        if (!req.body.password) {
-            return res.sendStatus(400)
-        }
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            const deleteUser = await User.destroy(
-                {
-                    where: {
-                        id: req.params.userId
-                    }
-                })
-                const token = req.headers?.authorization?.split(" ")[1];
-                if (!token) {
-                    res.sendStatus(403)
-                }
-                try {
-                    const data = jwt.verify(token, process.env.JWT_SECRET)
-                    if (data.id == req.params.userId) {
-                        res.status(201).json(deleteUser)
-                    }
-                } catch (err) {
-                    console.log(err);
-                    return res.status(403).json({ msg: "Invalid or missing token" })
-                }
-        } else {
-            res.sendStatus(403)
-        }
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
+	try {
+		const user = await User.findByPk(req.params.userId)
+		if (!user) {
+			res.sendStatus(404)
+		}
+		if (!req.body.password) {
+			return res.sendStatus(400)
+		}
+		if (await bcrypt.compare(req.body.password, user.password)) {
+			const deleteUser = await User.destroy(
+				{
+					where: {
+						id: req.params.userId
+					}
+				})
+			const token = req.headers?.authorization?.split(" ")[1];
+			if (!token) {
+				res.sendStatus(403)
+			}
+			try {
+				const data = jwt.verify(token, process.env.JWT_SECRET)
+				if (data.id == req.params.userId) {
+					res.status(201).json(deleteUser)
+				}
+			} catch (err) {
+				console.log(err);
+				return res.status(403).json({ msg: "Invalid or missing token" })
+			}
+		} else {
+			res.sendStatus(403)
+		}
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
 })
 
 //18
 router.get("/:userId/feed", async (req, res) => {
-    try {
-        const userId = 1;
-        const pageLength = parseInt(req.query["page-length"]) || 10;
-        const pageNumber = parseInt(req.query["page-number"]) || 0;
+	try {
+		const userId = 1;
+		const pageLength = parseInt(req.query["page-length"]) || 10;
+		const pageNumber = parseInt(req.query["page-number"]) || 0;
 
-        const user = await User.findByPk(req.params.userId, {
-            limit: pageLength,
-            offset: pageNumber * pageLength,
-            subQuery: false,
-            include: [{
-                model: Gallery,
-                as: "galleryFollowingUser",
-                include: [{
-                    model: Picture,
-                    attributes: ["id", "name", "S3URL"],
-                    include: [
-                        {
-                            model: User,
-                            attributes: ["displayName"]
-                        }, {
-                            model: Comment,
-                            attributes: ["id"]
-                        }, {
-                            model: Like,
-                            attributes: ["id", "delta"]
-                        }
-                    ]
-                }]
-            }, {
-                through: {
-                    attributes: []
-                },
-                model: User,
-                as: "userFollowingUser",
-                attributes: ["id", "displayName"],
-                include: [{
-                    model: Picture,
-                    attributes: ["id", "name", "S3URL"],
-                    include: [
-                        {
-                            model: Comment,
-                            attributes: ["id"]
-                        }, {
-                            model: Like,
-                            attributes: ["id", "delta"]
-                        }
-                    ]
-                }]
-            }]
-        })
-        const galleryPictures = user.galleryFollowingUser.reduce((pictures, gallery) => {
-            return pictures.concat(gallery.picture.map(picture => ({
-                id: picture.id,
-                name: picture.name,
-                commentCount: picture.comments.length,
-                imageURL: picture.S3URL,
-                score: picture.likes.reduce((score, { delta }) => score + delta, 0),
-                like: picture.likes.find(like => like.id === userId),
+		const user = await User.findByPk(req.params.userId, {
+			limit: pageLength,
+			offset: pageNumber * pageLength,
+			subQuery: false,
+			include: [{
+				model: Gallery,
+				as: "galleryFollowingUser",
+				include: [{
+					model: Picture,
+					attributes: ["id", "name", "S3URL"],
+					include: [
+						{
+							model: User,
+							attributes: ["displayName"]
+						}, {
+							model: Comment,
+							attributes: ["id"]
+						}, {
+							model: Like,
+							attributes: ["id", "delta"]
+						}
+					]
+				}]
+			}, {
+				through: {
+					attributes: []
+				},
+				model: User,
+				as: "userFollowingUser",
+				attributes: ["id", "displayName"],
+				include: [{
+					model: Picture,
+					attributes: ["id", "name", "S3URL"],
+					include: [
+						{
+							model: Comment,
+							attributes: ["id"]
+						}, {
+							model: Like,
+							attributes: ["id", "delta"]
+						}
+					]
+				}]
+			}]
+		})
+		const galleryPictures = user.galleryFollowingUser.reduce((pictures, gallery) => {
+			return pictures.concat(gallery.picture.map(picture => ({
+				id: picture.id,
+				name: picture.name,
+				commentCount: picture.comments.length,
+				imageURL: picture.S3URL,
+				score: picture.likes.reduce((score, { delta }) => score + delta, 0),
+				like: picture.likes.find(like => like.id === userId),
 
-            })))
-        }, []);
+			})))
+		}, []);
 
-        const pictures = user.userFollowingUser.reduce((pictures, users) => {
-            return pictures.concat(users.picture.map(picture => ({
-                id: picture.id,
-                name: picture.name,
-                commentCount: picture.comments.length,
-                imageURL: picture.S3URL,
-                score: picture.likes.reduce((score, { delta }) => score + delta, 0),
-                like: picture.likes.find(like => like.id === userId),
-            })));
-        }, []);
+		const pictures = user.userFollowingUser.reduce((pictures, users) => {
+			return pictures.concat(users.picture.map(picture => ({
+				id: picture.id,
+				name: picture.name,
+				commentCount: picture.comments.length,
+				imageURL: picture.S3URL,
+				score: picture.likes.reduce((score, { delta }) => score + delta, 0),
+				like: picture.likes.find(like => like.id === userId),
+			})));
+		}, []);
 
-        const feedPictures = galleryPictures.concat(pictures)
+		const feedPictures = galleryPictures.concat(pictures)
 
-        if (!user) {
-            res.sendStatus(404)
-        }
+		if (!user) {
+			res.sendStatus(404)
+		}
 
-        const responseJson = {
-            pageLength: pageLength,
-            pageNumber: pageNumber,
-            pictures: feedPictures
-        }
+		const responseJson = {
+			pageLength: pageLength,
+			pageNumber: pageNumber,
+			pictures: feedPictures
+		}
 
-        const token = req.headers?.authorization?.split(" ")[1];
-        if (!token) {
-            res.sendStatus(403)
-        }
-        try {
-            const data = jwt.verify(token, process.env.JWT_SECRET)
-            if (data.id == req.params.userId) {
-                res.status(201).json(responseJson)
-            } else {
-                res.sendStatus(403)
-            }
-        } catch (err) {
-            console.log(err);
-            return res.status(403).json({ msg: "Invalid or missing token" })
-        }
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
+		const token = req.headers?.authorization?.split(" ")[1];
+		if (!token) {
+			res.sendStatus(403)
+		}
+		try {
+			const data = jwt.verify(token, process.env.JWT_SECRET)
+			if (data.id == req.params.userId) {
+				res.status(201).json(responseJson)
+			} else {
+				res.sendStatus(403)
+			}
+		} catch (err) {
+			console.log(err);
+			return res.status(403).json({ msg: "Invalid or missing token" })
+		}
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
 })
 
 //20
 router.get("/:userId/profile", async (req, res) => {
-    try {
-        const userId = 1;
-        const picturesOnly = (req.query["pictures-only"] === 'true') || false;
-        const pageLength = parseInt(req.query["page-length"]) || 10;
-        const pageNumber = parseInt(req.query["page-number"]) || 0;
+	try {
+		const userId = 1;
+		const picturesOnly = (req.query["pictures-only"] === 'true') || false;
+		const pageLength = parseInt(req.query["page-length"]) || 10;
+		const pageNumber = parseInt(req.query["page-number"]) || 0;
 
-        const user = await User.findByPk(req.params.userId, {
-            limit: pageLength,
-            offset: pageNumber * pageLength,
-            subQuery: false,
-            attributes: picturesOnly ? [] : ["bio", "id", "displayName"],
-            include: [
-                ...(picturesOnly ? [] : [{
-                    through: {
-                        attributes: []
-                    },
-                    model: User,
-                    as: "userFollowingUser",
-                    attributes: ["id"]
-                }]),
-                {
-                    model: Picture,
-                    attributes: ["id", "name", "S3URL"],
-                    include: [
-                        {
-                            model: Comment,
-                            attributes: ["id"]
-                        }, {
-                            model: Like,
-                            attributes: ["id", "delta"]
-                        }
-                    ]
-                }
-            ]
-        })
+		const user = await User.findByPk(req.params.userId, {
+			limit: pageLength,
+			offset: pageNumber * pageLength,
+			subQuery: false,
+			attributes: picturesOnly ? [] : ["bio", "id", "displayName"],
+			include: [
+				...(picturesOnly ? [] : [{
+					through: {
+						attributes: []
+					},
+					model: User,
+					as: "userFollowingUser",
+					attributes: ["id"]
+				}]),
+				{
+					model: Picture,
+					attributes: ["id", "name", "S3URL"],
+					include: [
+						{
+							model: Comment,
+							attributes: ["id"]
+						}, {
+							model: Like,
+							attributes: ["id", "delta"]
+						}
+					]
+				}
+			]
+		})
 
-        const pictures = user.pictures.map(picture => ({
-            id: picture.id,
-            name: picture.name,
-            commentCount: picture.comments.length,
-            imageURL: picture.S3URL,
-            score: picture.likes.reduce((score, { delta }) => score + delta, 0),
-            like: picture.likes.find(like => like.id === userId),
-        }));
+		const pictures = user.pictures.map(picture => ({
+			id: picture.id,
+			name: picture.name,
+			commentCount: picture.comments.length,
+			imageURL: picture.S3URL,
+			score: picture.likes.reduce((score, { delta }) => score + delta, 0),
+			like: picture.likes.find(like => like.id === userId),
+		}));
 
-        if (!user) {
-            res.sendStatus(404)
-        }
-        res.json(picturesOnly ? { pictures: pictures } : {
-            id: user.id,
-            displayName: user.displayName,
-            bio: user.bio,
-            following: !!user.userFollowingUser.find(user => user.id === userId),
-            followerCount: user.userFollowingUser.length,
-            pictures: pictures
-        })
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
+		if (!user) {
+			res.sendStatus(404)
+		}
+		res.json(picturesOnly ? { pictures: pictures } : {
+			id: user.id,
+			displayName: user.displayName,
+			bio: user.bio,
+			following: !!user.userFollowingUser.find(user => user.id === userId),
+			followerCount: user.userFollowingUser.length,
+			pictures: pictures
+		})
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
 })
 
 module.exports = router;
